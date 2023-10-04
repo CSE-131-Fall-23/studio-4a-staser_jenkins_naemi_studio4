871d0c5f19fc769a8e779982fb852b37c4a921c5
henry <henry@Henry-Framework.dhcp.wustl.edu>
2023-10-04 14:27:57 -0500

    worked on studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -2,10 +2,20 @@ package studio4;
 
 import java.awt.Color;
 
+
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.rectangle(0.5, 0.5, 0.45, 0.3 );
+
+		
+		
+		
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,25 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		int num1 = in.nextInt();
+		int num2 = in.nextInt();
+		int num3 = in.nextInt();
+		boolean tf = in.nextBoolean();
+		double decimal1 = in.nextDouble();
+		double decimal2 = in.nextDouble();
+		double decimal3 = in.nextDouble();
+		double decimal4 = in.nextDouble();
+		StdDraw.setPenColor(num1, num2, num3);
+	
+		
+		StdDraw.square(decimal1, decimal2, decimal3);
+			
+			
+		
+		
+		
 	}
+	
+	
 }
